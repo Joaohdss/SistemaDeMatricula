6c3a5da1dc441f62ec18731b612b4d00b409c22d
DamiaoRobson <damiao.domiciano@ccc.ufcg.edu.br>
2018-07-28 20:14:34 -0300

    Ajudte de app.js

--- SistemaDeMatricula/src/main/resources/templates/app.js
+++ SistemaDeMatricula/src/main/resources/templates/app.js
@@ -1,11 +1,12 @@
 (function () {
-const app = angular.module('pre_matricula', ['$scope',($scope)
-    $scope.vm = {email='Email ccc',senha='Senha'}]);
+const app = angular.module('pre_matricula', []);
 
 app.controller('AppCtrl', AppCtrl);
 
 function AppCtrl() {
     const vm = this;
     vm.msg = "oi (vindo do controller)";
+    vm.email = "Email ccc";
+    vm.senha = "Senha";
   }
 }());
